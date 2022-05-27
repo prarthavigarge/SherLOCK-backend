ebde4c7f1be0f8c9f756d7e432b5156c83b330fb
prarthavi garge <prarthavi03@gmail.com>
2022-05-27 23:47:09 +0530

    danger level calculation with upload criminal route created

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const multer= require("multer");
 const mongoose= require("mongoose");
 const express = require("express");
 const Criminal = require("./models/criminal.js");
+const multerS3 = require("multer-s3");
 
 var s3 = new AWS.S3({
   accessKeyId: process.env.AWS_KEY_ID,
@@ -58,6 +59,27 @@ var upload = multer({
   },
 });
 
+var uploadS3 = multer({
+  storage: multerS3({
+    s3,
+    bucket: "prarthavi-bucket/sherLOCK",
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString() + "." + file.mimetype.split("/")[1]);
+    },
+  }),
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+  fileFilter: fileFilter,
+});
+
+const getAllCriminals = () =>{
+  return Criminal.find({});
+}
+
 app.get("/", (req, res) => {
   return res.status(200).json({
     message: "Server is up and running",
@@ -90,11 +112,11 @@ app.post("/findCriminal", upload.single("file"), async (req, res) => {
 
       const faceMatcher = new faceapi.FaceMatcher(results);
 
-      const criminals= ["WhatsApp Image 2022-05-27 at 7.19.54 PM.jpeg","WhatsApp Image 2022-05-27 at 7.22.43 PM.jpeg"];
+      const criminals= await getAllCriminals();
 
         let flag = true;
         for (let i = 0; i < criminals.length; i++) {
-          const key = criminals[i];
+          const key = criminals[i].imageKey;
           var url = s3.getSignedUrl("getObject", {
             Bucket: "prarthavi-bucket/sherLOCK",
             Key: key,
@@ -138,6 +160,90 @@ app.post("/findCriminal", upload.single("file"), async (req, res) => {
     });
   }
 })
+
+const getDangerLevel = (crimes) => {
+  let level = 0;
+  let lowLevelCrimes = 0;
+  let mediumLevelCrimes = 0;
+  let highLevelCrimes = 0;
+  for (let i = 0; i < crimes.length; i++) {
+    let crime = crimes[i].toLowerCase();
+    if (crime.includes("rape") || crime.includes("harrasement") || crime.includes("murder") || crime.includes("assault") || crime.includes("violence") || crime.includes("abuse") || crime.includes("kidnapping")) {
+      highLevelCrimes += 1;
+    }
+    if(crime.includes("fraud") || crime.includes("scam") || crime.includes("bribery") || crime.includes("theft") || crime.includes("burglary") || crime.includes("robbery") || crime.includes("embezzlement") || crime.includes("extortion")) {
+      mediumLevelCrimes += 1;
+    }
+    if(crime.includes("theft") || crime.includes("vandalism") || crime.includes("stolen goods") || crime.includes("drugs") || crime.includes("traffic") || crime.includes("trespassing")){
+      lowLevelCrimes += 1;
+    }
+  }
+  if(highLevelCrimes > 0) {
+    level = 90;
+  } else if(mediumLevelCrimes > 0) {
+    level = 40;
+    mediumLevelCrimes -= 1;
+  } else if(lowLevelCrimes > 0) {
+    level = 20;
+    lowLevelCrimes -= 1;
+  }
+
+  while(mediumLevelCrimes > 0 && level < 80) {
+      level += 10;
+      mediumLevelCrimes -= 1;
+  }
+
+  while(lowLevelCrimes > 0 && level < 60) {
+      level += 5;
+      lowLevelCrimes -= 1;
+  }
+
+  if(highLevelCrimes > 1){
+    level = 100;
+  }
+  return level;
+};
+
+app.post("/uploadCriminal", uploadS3.single("file"), async (req, res) => {
+  const { name, dob, address, crimes } = req.body;
+  if (!name || !dob || !address || !crimes) {
+    return res.status(400).json({
+      message: "Please provide all the details",
+    });
+  }
+  const dangerLevel = getDangerLevel(crimes);
+  try {
+    const uploadSingle = uploadS3.single("file");
+    uploadSingle(req, res, async (err) => {
+      if (err) {
+        console.log(err);
+        return res.send({
+          success: false,
+        });
+      } else {
+        console.log("Image uploaded successfully");
+        const criminal = new Criminal({
+          name,
+          dob,
+          address,
+          crimes,
+          dangerLevel,
+          imageKey: req.file.key,
+        });
+        await criminal.save();
+        return res.send({
+          message: "Criminal Added",
+          criminal
+        });
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: err.toString(),
+    });
+  }
+});
 
 const port = process.env.PORT || 3000;
 
